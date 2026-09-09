93b36e8b1c19861e0b24d9488840209714a4aee0
Kevin Delfour <kevin@delfour.co>
2026-09-09 21:15:29 +0200

    sim-gfx: ne pas versionner les artefacts de build
    131 fichiers du dossier build/ avaient été ajoutés par mégarde.

--- sim-gfx/build/CMakeFiles/vu-face.dir/home/kdelfour/Workspace/Contributions/meowkit-s3-firmware/lib/LovyanGFX/src/lgfx/v1/panel/Panel_M5HDMI.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/vu-face.dir/home/kdelfour/Workspace/Contributions/meowkit-s3-firmware/lib/LovyanGFX/src/lgfx/v1/panel/Panel_M5HDMI.cpp.o: \
- /home/kdelfour/Workspace/Contributions/meowkit-s3-firmware/lib/LovyanGFX/src/lgfx/v1/panel/Panel_M5HDMI.cpp \
- /usr/include/stdc-predef.h
